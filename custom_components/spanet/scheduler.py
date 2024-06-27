085a53620086364166ea0a188046c1b9f7bcfe36
Lloyd Weehuizen <lloyd@shift.net.nz>
2024-06-27 19:15:30 +1200

    Simplify scheduler

--- custom_components/spanet/scheduler.py
+++ custom_components/spanet/scheduler.py
@@ -1,13 +1,13 @@
-from datetime import datetime, timedelta
+import time
 
 class Task:
     def __init__(self, interval: int, callback):
         self.interval = interval
         self.callback = callback
-        self.next_tick = datetime.now()
+        self.next_tick = 0
 
     def trigger(self):
-        self.next_tick = datetime.now()
+        self.next_tick = 0
 
 
 class Scheduler:
@@ -21,8 +21,8 @@ class Scheduler:
         return task
 
     async def tick(self):
+        now = int(time.time())
         for task in self.tasks:
-            now = datetime.now()
             if task.next_tick <= now:
                 await task.callback()
-                task.next_tick = now + timedelta(seconds=task.interval)
+                task.next_tick = now + task.interval
